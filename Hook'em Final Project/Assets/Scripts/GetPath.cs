52d95917da9960a454f27f6441a51385768bddc5
LenaChu <a87192611@gmail.com>
2019-04-22 13:23:07 -0500

    Set up agent navigation

---
+++ Hook'em Final Project/Assets/Scripts/GetPath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetPath : MonoBehaviour
+{
+    public GameObject[] allPaths;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num = Random.Range(0, allPaths.Length);
+        transform.position = allPaths[num].transform.position;
+        MoveOnPathScript yourPath = GetComponent<MoveOnPathScript>();
+        yourPath.pathName = allPaths[num].name; 
+    }
+}
---
+++ Hook'em Final Project/Assets/Scripts/MoveOnPathScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnPathScript : MonoBehaviour
+{
+    public EditorPathScript PathToFollow;
+
+    public int CurrentWayPointID = 0;
+    public float speed;
+    private float reachDistance = 0.5f;
+    public float rotationSpeed = 5.0f;
+    public string pathName;
+
+    Vector3 last_position;
+    Vector3 current_position;
+
+
+
+
+    void Start()
+    {
+        PathToFollow = GameObject.Find(pathName).GetComponent<EditorPathScript>();
+        last_position = transform.position;
+        
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(PathToFollow.path_objs[CurrentWayPointID].position, transform.position);
+
+        //var rotation = Quaternion.LookRotation(PathToFollow.path_objs[CurrentWayPointID].position - transform.position);
+       //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+
+        transform.position = Vector3.MoveTowards(transform.position, PathToFollow.path_objs[CurrentWayPointID].position, Time.deltaTime * speed);
+        if(distance <= reachDistance)
+        {
+            CurrentWayPointID++;
+        }
+        if (CurrentWayPointID >= PathToFollow.path_objs.Count)
+        {
+            CurrentWayPointID = 0;
+        }
+    }
+}
